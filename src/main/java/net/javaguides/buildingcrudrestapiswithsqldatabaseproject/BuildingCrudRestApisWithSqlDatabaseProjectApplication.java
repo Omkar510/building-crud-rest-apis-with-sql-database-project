984b4fea41b50ab984b1bd552bd27057f68deb9c
Omkar510 <omkarsuvare49@gmail.com>
2023-09-17 18:48:26 +0530

    first commit

---
+++ src/main/java/net/javaguides/buildingcrudrestapiswithsqldatabaseproject/BuildingCrudRestApisWithSqlDatabaseProjectApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.buildingcrudrestapiswithsqldatabaseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingCrudRestApisWithSqlDatabaseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingCrudRestApisWithSqlDatabaseProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javaguides/buildingcrudrestapiswithsqldatabaseproject/BuildingCrudRestApisWithSqlDatabaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.buildingcrudrestapiswithsqldatabaseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingCrudRestApisWithSqlDatabaseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
